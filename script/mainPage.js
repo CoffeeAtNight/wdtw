cd6727d6fda437ef37ca35ec4c64f06f3e788683
Piotr Wójcik <krakownh1@gmail.com>
2020-06-01 22:35:53 +0100

    add animations to technologies section

--- script/mainPage.js
+++ script/mainPage.js
@@ -2,6 +2,12 @@ let aboutUs = document.getElementById('animationType1');
 let aboutLeft = document.querySelector('.aboutLeft');
 let aboutRight = document.querySelector('.aboutRight');
 
+let techno1 = document.getElementById('techno1');
+let techno2 = document.getElementById('techno2');
+let techno3 = document.getElementById('techno3');
+let techno4 = document.getElementById('techno4');
+
+
 window.addEventListener('scroll', () => {
     // pokazuje sekcje o nas
     if (window.scrollY > aboutUs.offsetTop + aboutUs.offsetHeight * 0.3) {
@@ -10,6 +16,13 @@ window.addEventListener('scroll', () => {
         aboutLeft.classList.add('active');
         aboutRight.classList.add('active');
     }
-
+    if (window.scrollY > techno1.offsetTop + techno1.offsetHeight * 0.5)
+        techno1.classList.add('active');
+    if (window.scrollY > techno2.offsetTop + techno2.offsetHeight * 0.5)
+        techno2.classList.add('active');
+    if (window.scrollY > techno3.offsetTop + techno3.offsetHeight * 0.5)
+        techno3.classList.add('active');
+    if (window.scrollY > techno4.offsetTop + techno4.offsetHeight * 0.5)
+        techno4.classList.add('active');
 });
 
